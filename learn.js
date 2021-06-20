d9cda6f40fe0aa76988dc146d8bb5a30aa324265
Suranjit Baroy <62200368+SuranjitBaroy@users.noreply.github.com>
2021-06-20 12:38:28 +0600

    Add files via upload
    JS Simple Project

---
+++ learn.js
@@ -0,0 +1,14 @@
+// JS Input Out Put Project
+
+
+
+
+var FirstName = prompt("Enter Your First Name");
+
+var LastName  = prompt("Enter Your Last  Name");
+
+
+
+
+
+console.log(FirstName + " " +  LastName);
